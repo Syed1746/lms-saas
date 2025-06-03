d0503d71dc0d960f60ddaf80cfd1dd6a2985107b
syed1746 <syedsameernazeer@gmail.com>
2025-06-03 23:50:43 +0530

    updated the clerk and supabase implementation and clerk implementation done

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,6 @@ export default clerkMiddleware(async (auth, req) => {
     await auth.protect()
   }
 })
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
